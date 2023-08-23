c5ba1e761050ee50ba738053a76b911e4cef3a7c
tsklipp <telmo.klipp@gmail.com>
2023-08-23 13:51:47 -0300

    Update scripts

--- scripts/tidyingdata_1.R
+++ scripts/tidyingdata_1.R
@@ -14,17 +14,15 @@ dt <- dt |>
 dt <- dt |>
   dplyr::mutate(sexo = as.factor(sexo), grupo_idade = as.factor(grupo_idade))
 
-# Ordenando manualmente os níveis do tipo 'factor' da coluna grupo_idade
+# Criando uma ordenação manual para os níveis do tipo 'factor' da coluna grupo_idade
 faixa_etaria <- c("0 a 4 anos", "5 a 9 anos", "5 a 13 anos", "10 a 13 anos", "14 a 15 anos",
                   "14 a 17 anos", "16 a 17 anos",    "18 a 19 anos",    "20 a 24 anos",
                   "25 a 29 anos", "30 a 39 anos", "40 a 49 anos", "50 a 59 anos",
                   "60 a 64 anos", "60 anos ou mais", "65 anos ou mais")
-#faxa_etaria <- c("0 a 4\n anos", "5 a 9\n anos", "5 a 13\n anos", "10 a 13\n anos",
-#                 "14 a 15 \n anos",    "14 a 17\n anos",    "16 a 17\n anos",
-#                 "18 a 19\n anos" ,   "20 a 24\n anos" ,   "25 a 29\n anos" ,
-#                 "30 a 39\n anos",    "40 a 49\n anos", "50 a 59\n anos"  ,
-#                 "60 a 64\n anos"  ,  "60 anos\n ou mais", "65 anos\n ou mais")
-levels(dt$grupo_idade) <- faixa_etaria
+
+# Aplicando a ordenação manual ao converter novamente a variável para o tipo 'factor', mas informando os níveis
+dt <- dt |>
+  dplyr::mutate(grupo_idade = factor(grupo_idade, levels = faixa_etaria))
 
 # Salvar o novo data frame, agora com dados já no formato tidy, para posterior uso
 readr::write_csv(dt, "dados/popupacao_brasil_pnadc_tidy.csv")
@@ -36,4 +34,4 @@ dt |>
 
 dt |>
   dplyr::filter(!(grupo_idade %in% c('5 a 13 anos', '14 a 17 anos', '60 anos ou mais'))) |>
-  dplyr::summarise(maximo = max(populacao), minimo = min(populacao), media = mean(populacao), desvio_padrao = sd(populacao), .by = c(grupo_idade))
+  dplyr::summarise(minimo = min(populacao), media = mean(populacao), maximo = max(populacao), desvio_padrao = sd(populacao), .by = c(grupo_idade))
